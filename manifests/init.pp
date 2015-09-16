da6994b468abe1cea5b4e6faf8b983c1ce7f3a9e
ananthakrishab5 <ananthakrishna.paturi@gmail.com>
2015-09-16 15:30:10 -0500

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,13 +2,5 @@ class useradd{
 	user{'testpuppet':
 		ensure => 'present',
 		uid => '3000',
-		home => '/home/testpuppet',
-		comment => 'Pupppet testing',
-		gid => 'test',
-#		password => ' ',
-}	
- 	group{'test':
- 		ensure => 'present',
-		gid => '3000',
 }
 }
